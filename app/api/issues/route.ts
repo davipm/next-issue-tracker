ba91e330232a66f9c12a576a65467a7074896fa1
Davi Pereira <davi.p.m94@gmail.com>
2024-03-30 14:28:56 -0300

    fix db

--- app/api/issues/route.ts
+++ app/api/issues/route.ts
@@ -1,26 +1,25 @@
 import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/lib/db";
+import { db } from "@/lib/db";
 import authOptions from "@/lib/auth";
 import { issueSchema } from "@/schema/validationSchema";
 import { getServerSession } from "next-auth";
 
 export default async function POST(req: NextRequest) {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
-  if (!session) return NextResponse.json({}, { status: 401 })
+  if (!session) return NextResponse.json({}, { status: 401 });
 
-  const body = await req.json()
-  const validation = issueSchema.safeParse(body)
+  const body = await req.json();
+  const validation = issueSchema.safeParse(body);
 
-  if (!validation.success)
-    return NextResponse.json(validation.error.format(), { status: 400 })
+  if (!validation.success) return NextResponse.json(validation.error.format(), { status: 400 });
 
-  const newIssues = await prisma.issue.create({
+  const newIssues = await db.issue.create({
     data: {
       title: body.title,
       description: body.description,
-    }
-  })
+    },
+  });
 
-  return NextResponse.json(newIssues, { status: 201 })
+  return NextResponse.json(newIssues, { status: 201 });
 }
