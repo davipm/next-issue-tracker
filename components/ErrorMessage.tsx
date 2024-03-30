42c99105b3b4d1c0611810d730450112df84baf5
Davi Pereira <davi.p.m94@gmail.com>
2024-03-30 00:59:17 -0300

    init

---
+++ components/ErrorMessage.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { Text } from '@radix-ui/themes'
+import { PropsWithChildren } from "react";
+
+
+export function ErrorMessage({ children }: PropsWithChildren) {
+  if (!children) return null;
+  return <Text color="red" as="p">{children}</Text>
+}
---
+++ lib/auth.ts
@@ -0,0 +1,20 @@
+import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import prisma from "@/lib/db";
+import { NextAuthOptions } from "next-auth";
+
+const authOptions: NextAuthOptions = {
+// @ts-ignore
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  }
+}
+
+export default authOptions;
