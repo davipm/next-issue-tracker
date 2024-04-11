7e467d99a67b1e71d554fe433a1b1aadd240e1f2
Davi Pereira <davi.p.m94@gmail.com>
2024-04-11 12:47:59 -0300

    adding page

---
+++ app/issues/[id]/assignee-select.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+export function AssigneeSelect() {
+  const [item, setItem] = useState(null);
+
+  return (
+    <div>
+      <p>AssigneeSelect</p>
+    </div>
+  );
+}
---
+++ app/issues/[id]/issue-details.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { useState } from "react";
+
+// import { Container } from "./styles";
+
+export function IssueDetails() {
+  const [item, setItem] = useState(null);
+
+  return (
+    <div>
+      <p>IssueDetails</p>
+    </div>
+  );
+}
