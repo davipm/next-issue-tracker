4de336612bd9a3a7d7971edcf7d7f58448052042
Davi Pereira <davi.p.m94@gmail.com>
2024-04-21 19:06:07 +0900

    adding issue status

---
+++ components/issue-status-filter.tsx
@@ -0,0 +1,47 @@
+"use client";
+
+import { Status } from "@prisma/client";
+import { useRouter, useSearchParams } from "next/navigation";
+import { Select } from "@radix-ui/themes";
+
+type State = {
+  label: string;
+  value?: Status;
+}[];
+
+const statuses: State = [
+  { label: "All" },
+  { label: "Open", value: Status.OPEN },
+  { label: "In Progress", value: Status.IN_PROGRESS },
+  { label: "Closed", value: Status.CLOSED },
+];
+
+export function IssueStatusFilter() {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+
+  return (
+    <Select.Root
+      defaultValue={searchParams.get("status") || ""}
+      onValueChange={(status) => {
+        const params = new URLSearchParams();
+        if (status) params.append("status", status);
+        if (searchParams.get("orderBy")) params.append("orderBy", searchParams.get("orderBy")!);
+
+        const query = params.size ? "?" + params.toString() : "";
+        router.push(`/issues/list/${query}`);
+      }}
+    >
+      <Select.Trigger placeholder="Filter by status..." />
+      <Select.Content>
+        <Select.Group>
+          {statuses.map((status) => (
+            <Select.Item key={status.value} value={status.value!}>
+              {status.label}
+            </Select.Item>
+          ))}
+        </Select.Group>
+      </Select.Content>
+    </Select.Root>
+  );
+}
